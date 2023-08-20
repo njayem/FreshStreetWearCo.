bdde13c302e36704de7a41c1bd945b14d681b064
njayem <nadine.el.mufti@gmail.com>
2023-08-20 02:14:41 -0400

    Set up CRA

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React from "react";
+import "./categories.styles.scss";
+import CategoryItem from "./components/category-item/category-item.component.jsx";
+const App = () => {
+	const categories = [
+		{
+			id: 1,
+			title: "hats",
+			imageUrl:
+				"https://raw.githubusercontent.com/njayem/FreshStreetWearCo./main/public/assets/Hats.png",
+		},
+		{
+			id: 2,
+			title: "jackets",
+			imageUrl:
+				"https://raw.githubusercontent.com/njayem/FreshStreetWearCo./main/public/assets/Jackets.png",
+		},
+		{
+			id: 3,
+			title: "sneakers",
+			imageUrl:
+				"https://raw.githubusercontent.com/njayem/FreshStreetWearCo./main/public/assets/Sneakers.png",
+		},
+		{
+			id: 4,
+			title: "womens",
+			imageUrl:
+				"https://raw.githubusercontent.com/njayem/FreshStreetWearCo./main/public/assets/Womens.png",
+		},
+		{
+			id: 5,
+			title: "mens",
+			imageUrl:
+				"https://raw.githubusercontent.com/njayem/FreshStreetWearCo./main/public/assets/Mens.png",
+		},
+	];
+
+	return (
+		<div className="categories-container">
+			{/* Inside our Categories Container */}
+			{categories.map(({ category }) => {
+				return (
+					<CategoryItem
+						key={category.id}
+						title={category.title}
+						imageUrl={category.imageUrl}
+					/>
+				);
+			})}
+			;
+		</div>
+	);
+};
+
+export default App;
